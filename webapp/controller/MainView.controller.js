9f82df4940da45562d2a1b505fa62dbc5eb93005
JaimeRamirez1234 <jaimeramirez@alfasolucionesti.com>
2024-07-04 18:28:08 +0000

    First Commit 04

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,12 +1,25 @@
+
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
 function (Controller) {
     "use strict";
 
-    return Controller.extend("alfa02.alfa02.controller.MainView", {
-        onInit: function () {
+    var Main = Controller.extend("alfa02.alfa02.controller.MainView", {});
 
-        }
-    });
+    Main.prototype.onValidate = function(){
+            var inputEmployee = this.byId("inputEmployee");
+            var valueEmployee = inputEmployee.getValue();
+            
+            if(valueEmployee.length === 6){
+                //inputEmployee.setDescription("OK");
+                this.getView().byId("labelCountry").setVisible(true);
+                this.getView().byId("slCountry").setVisible(true);
+            }else{
+                //inputEmployee.setDescription("Not OK");
+                this.getView().byId("labelCountry").setVisible(false);
+                this.getView().byId("slCountry").setVisible(false);
+            }
+        };
+    return Main;
 });
