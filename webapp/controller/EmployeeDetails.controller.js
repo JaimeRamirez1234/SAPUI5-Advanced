e158e5cd808830cf319e33393786c03967dd4015
JaimeRamirez1234 <jaimeramirez@alfasolucionesti.com>
2024-07-25 18:21:20 +0000

    First commit 13

--- webapp/controller/EmployeeDetails.controller.js
+++ webapp/controller/EmployeeDetails.controller.js
@@ -20,28 +20,17 @@ function(Controller, formatter) {
         newIncidence.bindElement("incidenceModel>/" + index);
         tableIncidence.addContent(newIncidence);
     };
+
+
     function onDeleteIncidence(oEvent){
 
-        var tableIncidence = this.getView().byId("tableIncidence");
-        var rowIncidence = oEvent.getSource().getParent().getParent();
-        var incidenceModel = this.getView().getModel("incidenceModel");
-        var odata = incidenceModel.getData();
-        var contextObject = rowIncidence.getBindingContext("incidenceModel");
-
-        odata.splice(contextObject.index - 1, 1);
-        for(var i in odata){
-            odata[i].index = parseInt(i) + 1;
-
-        }
-
-        incidenceModel.refresh();
-
-        tableIncidence.removeContent(rowIncidence);
-
-        for(var j in tableIncidence.getContent()){
-            tableIncidence.getContent()[j].bindElement("incidenceModel>/" + j);
-        }
-
+        var contextObj = oEvent.getSource().getBindingContext("incidenceModel").getObject();
+        this._bus.publish("incidence", "onDeleteIncidence", {
+            IncidenceId: contextObj.IncidenceId,
+            SapId : contextObj.SapId,
+            EmployeeId : contextObj.EmployeeId
+        } );
+       
     };
 
     function onSaveIncidence(oEvent) {
@@ -51,6 +40,22 @@ function(Controller, formatter) {
         this._bus.publish("incidence", "onSaveIncidence", { incidenceRow : incidenceRow.sPath.replace('/', '') } );
     };
 
+    function updateIncidenceCreationDate(oEvent){
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.CreationDateX = true;
+    };
+    function updateIncidenceReason(oEvent){
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.ReasonX = true;
+    };
+    function updateIncidenceType(oEvent){
+        var context = oEvent.getSource().getBindingContext("incidenceModel");
+        var contextObj = context.getObject();
+        contextObj.TypeX = true;
+    };
+
     var EmployeeDetails = Controller.extend("alfa02.alfa02.controller.EmployeeDetails", {});
 
     EmployeeDetails.prototype.onInit = onInit;
@@ -58,6 +63,10 @@ function(Controller, formatter) {
     EmployeeDetails.prototype.onDeleteIncidence = onDeleteIncidence;
     EmployeeDetails.prototype.Formatter = formatter;
     EmployeeDetails.prototype.onSaveIncidence = onSaveIncidence;
+    EmployeeDetails.prototype.updateIncidenceCreationDate = updateIncidenceCreationDate;
+    EmployeeDetails.prototype.updateIncidenceReason = updateIncidenceReason;
+    EmployeeDetails.prototype.updateIncidenceType = updateIncidenceType;
+
     return EmployeeDetails;
 
 });
